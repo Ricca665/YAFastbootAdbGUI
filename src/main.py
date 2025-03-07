c36b7cf642d6ef84878880ad1217396aac6c5b10
Ricca665 <riccardosarti2010@gmail.com>
2025-03-07 18:15:47 +0100

    added more stuff

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
-import os
 import dearpygui.dearpygui as dpg
 
-from utils import *
+from utils import * #Almost every single thing it does
 
+#Default values for radio buttons
 isfastboot = False
 radio = "Normal"
 
+#DearPyGUI initialization stuff
 dpg.create_context()
-
-dpg.create_viewport(title="YA ADB/Fastboot GUI", width=500, height=500)
-
+dpg.create_viewport(title="YA ADB/Fastboot GUI", width=450, height=530)
 dpg.setup_dearpygui()
 
+#RADIO BUTTONS ONLY
+#TODO: Rewrite this shit of a radio input :sob:
 def _log_radio(sender, app_data, user_data):
     global radio
     radio = app_data
@@ -21,21 +22,32 @@ def _isfastboot(sender, app_data, user_data):
     global isfastboot
     isfastboot = app_data
     return isfastboot
+#END OF CRAP CODE
 
+#We initialize the buttons and other stuff
 with dpg.window(tag="primary"):
     dpg.add_text("Devices functions:", )
     dpg.add_button(label="Get connected devices", callback=lambda: get_devices("log"))
+    dpg.add_button(label="Get state of device", callback=lambda: get_info("state", "log"))
+    dpg.add_button(label="Get serial number", callback=lambda: get_info("sn", "log"))
 
     dpg.add_spacer(height=10)
     dpg.add_text("Reboot options:", )
-    dpg.add_button(label="Reboot", callback=lambda: reboot(radio, isfastboot))
+    dpg.add_button(label="Reboot", callback=lambda: reboot(radio, isfastboot)) #We use the data to tell what mode to reboot the phone is
     dpg.add_radio_button(("Normal", "Recovery", "Fastboot", "EDL"), callback=_log_radio, horizontal=True)
-    dpg.add_checkbox(label="Is in fastboot?", callback=_isfastboot)
-   
-    with dpg.child_window(tag="log", width=400, height=200):
-        pass  # empty log window
+    dpg.add_checkbox(label="Is it in fastboot?", callback=_isfastboot)
+    dpg.add_spacer(height=10)
+    dpg.add_button(label="Unlock bootloader", callback=lambda: unlock("log"))
+    dpg.add_button(label="Lock bootloader", callback=lambda: lock("log"))
+    dpg.add_spacer(height=50)
+    with dpg.child_window(tag="log", width=400, height=200): #Log window
+        dpg.add_text("Click some buttons for output!", parent="log") #Added this to greet the user, it's gonna appear once
 
+
+#Finishing initialization by viewing our window
 dpg.show_viewport()
-dpg.set_primary_window("primary", True)
-dpg.start_dearpygui()
+dpg.set_primary_window("primary", True) #Setting it to primary
+dpg.start_dearpygui() #Actually starting dearpygui functions
+
+#Destroying when closing :sob:
 dpg.destroy_context()
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,15 @@ import os
 import subprocess
 import dearpygui.dearpygui as dpg
 
-def reboot(type, isfastboot):
+def log_text(output, message): #Log text to console
+    dpg.add_text(message, parent=output)
+    dpg.set_y_scroll(output, 999999)  # Scroll to bottom
+
+"""Mess of a code, used to reboot the phone into various modes (for and and fastboot) and also
+   hide the output by setting the capture output flag to True, so it tries to print to a variable but it can't
+   Since we setted it to tell it to try and print it to the variable and NOT to the console
+   I know it's a hacky hack but i don't care as long as it works"""
+def reboot(type, isfastboot): 
     if type == "Normal" and isfastboot == False: #omfg dearpygui thank you for existing, and being simple to use
         subprocess.run(["adb", "reboot"], capture_output=True)
     elif type == "Recovery" and isfastboot == False:
@@ -10,7 +18,7 @@ def reboot(type, isfastboot):
     elif type == "Fastboot" and isfastboot == False:
         subprocess.run(["adb", "reboot", "bootloader"], capture_output=True)
     elif type == "EDL" and isfastboot == False:
-        subprocess.run(["adb", "reboot", "edl"], capture_output=True)
+        subprocess.run(["adb", "reboot", "edl"], capture_output=True) #Not many devices support this, adding in case
     
     if type == "Normal" and isfastboot == True:
         subprocess.run(["fastboot", "reboot"], capture_output=True)
@@ -20,24 +28,56 @@ def reboot(type, isfastboot):
         subprocess.run(["fastboot", "reboot-bootloader"], capture_output=True)
     elif type == "EDL" and isfastboot == True:
         subprocess.run(["fastboot", "reboot", "edl"], capture_output=True) #Not many devices support this, adding in case
-    
 
-def get_info(type):
+"""
+ Unlocking and locking the bootloader
+ I have to capcture both stdout AND stderr, not having realised that fastboot uses both
+"""
+def unlock(log_window):
+    dpg.delete_item("log", children_only=True)
+    result_fastboot_unlock = subprocess.run(["fastboot", "oem", "unlock"], capture_output=True, text=True)
+    result = result_fastboot_unlock.stdout.strip().split("\n")
+    result_err = result_fastboot_unlock.stderr.strip().split("\n")
+    for line in result:
+        log_text(log_window, line)
+
+    for line in result_err:
+        log_text(log_window, line)
+
+def lock(log_window):
+    dpg.delete_item("log", children_only=True)
+    result_fastboot_lock = subprocess.run(["fastboot", "oem", "lock"], capture_output=True, text=True)
+    result = result_fastboot_lock.stdout.strip().split("\n")
+    result_err = result_fastboot_lock.stderr.strip().split("\n")
+    for line in result:
+        log_text(log_window, line)
+        
+    for line in result_err:
+        log_text(log_window, line)
+"""End of bootloader unlocking"""
+
+def get_info(type, log_window):
+    """Get state, based on type"""
     if type == "state":
-        os.system("adb get-state device")
-    elif type == "s": #Serial number
-        os.system("adb get-serialno")
+        info = subprocess.run(["adb", "get-state", "device"], capture_output=True, text=True)
+    elif type == "sn": #Serial number
+        info = subprocess.run(["adb", "get-serialno"], capture_output=True, text=True)
 
-def restart_adb_server():
+    """Print it to log window"""
+    dpg.delete_item("log", children_only=True)
+    result = info.stdout.strip().split("\n")
+    result_err = info.stderr.strip().split("\n")
+    for line in result:
+        log_text(log_window, line)
+        
+    for line in result_err:
+        log_text(log_window, line)
+
+def restart_adb_server(): #Not implemented, yet...
     os.system("adb kill-server")
     os.system("adb start-server")
 
-def log_text(output, message):
-
-    dpg.add_text(message, parent=output)
-    dpg.set_y_scroll(output, 999999)  # Scroll to bottom
-
-def get_devices(log_window):
+def get_devices(log_window): #Get devices both from and and fastboot
     dpg.delete_item("log", children_only=True)
     adb_result = subprocess.run(["adb", "devices"], capture_output=True, text=True)
     devices_list = adb_result.stdout.strip().split("\n")
@@ -50,5 +90,5 @@ def get_devices(log_window):
     for line in fastboot_devices_list:
         log_text(log_window, line)
 
-def shell():
+def shell(): #Not yet implemented...
     os.system("adb shell")
